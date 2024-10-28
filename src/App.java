7a80871eb9c072db6d3d7bb3b72c3f120096500c
Stigdogg <valtteri.virta@student.hamk.fi>
2024-10-28 09:41:09 +0200

    Tulostin tehtävän aloitus

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        System.out.println("Hei olen Tulostin-ohjelma");
         
+        String tekijä = "Valtteri";
+
+        System.out.println("Ohjelman tekijä: " + tekijä);
+
+        int luku1 = 5;
+        int luku2 = 2;
+        int tulo = luku1 * luku2;
+
+        System.out.println (luku1 + " * " + luku2 + " = " + tulo);
+
+        //System.out.println("Luku2 muuttujan arvo: " + luku2);
+
+        //System.out.println("Luku 1: " + luku1);
     }
 }
