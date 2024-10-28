13f72c31f2ca6f9cb992f30beaa307d88fda2e69
Stigdogg <valtteri.virta@student.hamk.fi>
2024-10-28 09:44:04 +0200

    Vaihdettiin muuttujat double muuttujiksi
    Lisättiin myös laskutoimituksia

--- src/App.java
+++ src/App.java
@@ -6,14 +6,18 @@ public class App {
 
         System.out.println("Ohjelman tekijä: " + tekijä);
 
-        int luku1 = 5;
-        int luku2 = 2;
-        int tulo = luku1 * luku2;
+        double luku1 = 5;
+        double luku2 = 2;
+        double tulo = luku1 * luku2;
+        double summa = luku1 + luku2;
+        double erotus = luku1 - luku2;
+        double jako = luku1 / luku2;
+
 
         System.out.println (luku1 + " * " + luku2 + " = " + tulo);
 
         //System.out.println("Luku2 muuttujan arvo: " + luku2);
 
-        //System.out.println("Luku 1: " + luku1);
+        //System.out.println("Luku1 muuttujan arvo: " + luku1);
     }
 }
