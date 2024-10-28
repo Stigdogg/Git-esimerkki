bc4e6d8992ab2b6cccac34f4fa063a76159c4a88
Stigdogg <valtteri.virta@student.hamk.fi>
2024-10-28 09:45:42 +0200

    Siivottiin koodia ja lisättiin puuttuvat tulosteet

--- src/App.java
+++ src/App.java
@@ -1,23 +1,28 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hei olen Tulostin-ohjelma");
-        
-        String tekijä = "Valtteri";
-
-        System.out.println("Ohjelman tekijä: " + tekijä);
-
         double luku1 = 5;
         double luku2 = 2;
         double tulo = luku1 * luku2;
         double summa = luku1 + luku2;
         double erotus = luku1 - luku2;
         double jako = luku1 / luku2;
+        
+        
+        
+        System.out.println("Hei olen Tulostin-ohjelma");
+        
+        String tekijä = "Valtteri";
+
+        System.out.println("Ohjelman tekijä: " + tekijä);
+
+        System.out.println("Luku1 muuttujan arvo: " + luku1);
+        System.out.println("Luku2 muuttujan arvo: " + luku2);        
 
 
         System.out.println (luku1 + " * " + luku2 + " = " + tulo);
 
-        //System.out.println("Luku2 muuttujan arvo: " + luku2);
+        
 
-        //System.out.println("Luku1 muuttujan arvo: " + luku1);
+        
     }
 }
